48a3fc3d58cad98c1935cc6b9e510541cc8debcc
rcikaym <renewbie26@gmail.com>
2024-09-18 15:17:53 +0700

    update I

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { render } from "react-dom";
-import { Button, DatePicker, message } from "antd";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
