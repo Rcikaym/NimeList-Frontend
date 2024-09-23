bd0608ea42d8b8364fc2b8da95fed48ab6cbc764
Rcikaym <renewbie26@gmail.com>
2024-09-23 14:12:37 +0700

    several changes with the fetch system

--- src/components/DisplayLongText.tsx
+++ src/components/DisplayLongText.tsx
@@ -2,7 +2,7 @@ import React, { memo } from "react";
 
 // Extracted to separate components for better performance
 const DisplayLongText = memo(({ text }: { text: string }) => (
-  <div className="text-gray-600 tracking-wide leading-relaxed">
+  <div className="tracking-wide leading-relaxed">
     {text.split(/(?:\r\n|\r|\n)/g).map((line, index) => (
       <React.Fragment key={index}>
         {line}
